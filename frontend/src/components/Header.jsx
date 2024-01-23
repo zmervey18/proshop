8422cfffbbdb10b4b4d82c6608f111f25f8d9adb
zmervey18 <zmervey18@gmail.com>
2024-01-23 11:13:02 +0000

    Frontend initial

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import {LinkContainer} from "react-router-bootstrap"
+import logo from '../assets/logo.png'
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+      <header>
+          <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+              <Container>
+                  <LinkContainer to="/">
+                    <Navbar.Brand>
+                        <img src={logo} alt="ProShop"/>
+                        ProShop
+                        </Navbar.Brand>
+                     </LinkContainer>
+                  <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
+                  <Navbar.Collapse id="basic-navbar-nav">
+                      <Nav className='ms-auto'>
+                          <LinkContainer to="cart">
+                              <Nav.Link><FaShoppingCart /></Nav.Link>
+                          </LinkContainer>
+                          <LinkContainer to="/login">
+                              <Nav.Link ><FaUser /></Nav.Link>
+                            </LinkContainer>
+                      </Nav>
+                  </Navbar.Collapse>
+              </Container>
+          </Navbar>
+    </header>
+  )
+}
+
+export default Header
